38f63e92cb674829c979624337d6875c529dd0f9
Ivo.araujo <86474505+ivoaraujo17@users.noreply.github.com>
2022-11-17 20:48:27 -0300

    Visão geral da construção da interface

---
+++ interface.c
@@ -0,0 +1,14 @@
+/*Interface principal
+    1-Selecionar temporada
+    2-Visao Geral
+
+  Interface Temporada
+    1-Selecionar Episodio
+    2-Visao Geral da Temporada
+    3-Voltar
+  
+  Interface Episodio
+    Resumo do episodio
+    1-Voltar para temporada
+    2-Voltar para Tela inicial
+*/
