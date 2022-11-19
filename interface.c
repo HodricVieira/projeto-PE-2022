4bdd02dc5d7d6b18b61a200d5af46e69461ae39e
HodricVieira <rvo2@academico.ufpb.br>
2022-11-19 18:42:54 -0300

    interface principal melhorada

--- interface.c
+++ interface.c
@@ -1,27 +1,52 @@
 #include <stdio.h>
 #include "funcoes.h"
 
+
+int interf_temp(int tempr){
+
+}
+
 int main(){
-int ip;
-bool close;
+int ip, temp;
+bool closeip, closetemp;
 do{
-printf("Digite 1 para selecionar a temporada ou digite 2 para ver a visão geral ou 3 para fechar o programa./n");
+printf("Digite 1 para ter a visão geral, 2 para escolher a temporada para visualizar ou 0 para fechar o programa/n");
 scanf("%d", &ip);
   switch (ip)
   {
-  case 1:
+  case 0: closeip = TRUE;
     break;
   
-  case 2:
+  case 1: /* Função da visão geral */
     break;
   
-  case 3: close = TRUE
+  case 2: 
+    do{
+    printf("Digite o numero da temporada que deseja ver:/n
+            1 temporada/n
+            2 temporada/n
+            3 temporada/n
+            4 temporada/n
+            5 temporada/n
+            6 temporada/n
+            7 temporada/n
+            8 temporada/n
+            Digite 0 para voltar");
+    scanf("%d", &temp);
+    if (temp >= 1 or temp <= 8)
+      interf_temp(temp);
+    else if (temp == 0)
+      closetemp = TRUE;
+    else 
+      printf("Comando invalido");
+    
+    } while (closetemp != TRUE);
     break;
   
-  default: printf("Comando Invalido digite novamente./n")
+  default: printf("Comando Invalido digite novamente./n");
     break;
   }
-  }while (close != TRUE);
+  }while (closeip != TRUE);
 }
 /*Interface principal
     1-Selecionar temporada
