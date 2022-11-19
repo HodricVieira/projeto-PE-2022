e6c93ca0bebe0d47e3288cb94fb5fa6bb5319591
HodricVieira <rvo2@academico.ufpb.br>
2022-11-19 20:10:36 -0300

    interface feita, não testada
    Falta testar e revisar o código

--- interface.c
+++ interface.c
@@ -1,19 +1,76 @@
 #include <stdio.h>
 #include "funcoes.h"
 
+int ver_ep(int eps, int temp){
+  int volta;
+  bool close_ver_ep;
 
-int interf_temp(int tempr){
-
+  /* Funcao para o resumo do episodio*/
+  
+  do {
+  printf("Digite 0 para voltar a tela principal ou 1 para voltar para seleção de episodios:/n");
+  scanf("%d", &volta);
+  if (volta == 0 or volta == 1)
+    close_ver_ep = TRUE;
+  else 
+    printf("Comando invalido digite novamente./n");
+  }while (close_ver_ep != TRUE);
+  return volta;
 }
 
+/* ##################################################################################### */
+int interf_temp(int tempr){
+  int it, ep_max, vp;
+  bool closeit, closeep;
+  do{
+    printf("Digite 1 para ter a visão geral da temporada, 2 para escolher o episodio para visualizar ou 0 para voltar:/n");
+    scanf("%d", &it);
+    switch (it){
+    case 0: closeit = TRUE;
+      break;
+    
+    case 1: /* Função da visão geral */
+      break;
+    
+    case 2: 
+      do {
+      if (tempr == 7){
+        printf("Digite o numero do episodio de 1 a 7 ");
+        ep_max = 7}
+      else if (tempr == 8){
+        printf("Digite o numero do episodio de 1 a 6 ");
+        ep_max = 6}
+      else{
+        printf("Digite o numero do episodio de 1 a 10 ");
+        ep_max = 10}
+      printf("ou digite 0 para voltar/n");
+      scanf("%d", &ep);
+      if (ep >= 1 or ep <= ep_max){
+        vp = ver_ep(ep, tempr);
+        if (vp == 0)
+          closeep = TRUE;
+          closeit = TRUE;
+      }else if (ep == 0)
+        closeep = TRUE;
+      else 
+        printf("Comando invalido digite novamente./n");
+      } while (closeep != TRUE);
+      break;
+
+    default: printf("Comando Invalido digite novamente./n");
+      break;
+  }while (closeit != TRUE);
+  return;
+}
+}
+/* ############################################################################## */
 int main(){
-int ip, temp;
+int ip, temprd;
 bool closeip, closetemp;
 do{
 printf("Digite 1 para ter a visão geral, 2 para escolher a temporada para visualizar ou 0 para fechar o programa/n");
 scanf("%d", &ip);
-  switch (ip)
-  {
+  switch (ip){
   case 0: closeip = TRUE;
     break;
   
@@ -32,13 +89,13 @@ scanf("%d", &ip);
             7 temporada/n
             8 temporada/n
             Digite 0 para voltar");
-    scanf("%d", &temp);
-    if (temp >= 1 or temp <= 8)
+    scanf("%d", &temprd);
+    if (temprd >= 1 or temprd <= 8)
       interf_temp(temp);
-    else if (temp == 0)
+    else if (temprd == 0)
       closetemp = TRUE;
     else 
-      printf("Comando invalido");
+      printf("Comando invalido digite novamente./n");
     
     } while (closetemp != TRUE);
     break;
