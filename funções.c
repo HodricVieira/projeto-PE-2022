615dca12770494b9bdbc734b9e373c16b0c5857b
Ivo.araujo <86474505+ivoaraujo17@users.noreply.github.com>
2022-11-20 10:55:01 -0300

    Interface criada

--- funções.c
+++ funções.c
@@ -1,2 +1,15 @@
 #include <stdio.h>
 /*Aqui coloquem o codigo das funções como normalmente*/
+
+/*Função pra buscar resumo geral, deve receber o array de structs dos episodios*/
+
+/*Função pra buscar resumo da temporada, deve receber o array de structs dos episodios*/
+
+/*Função que busca e printa todas as temporadas
+  aqui será criada um função que vai ler todas as structs e vai printando as temporadas
+*/
+
+/*Função que busca e printa todas os episodios
+  aqui será criada um função que vai ler todas as structs e vai printando os numeros e nome
+*/
+
