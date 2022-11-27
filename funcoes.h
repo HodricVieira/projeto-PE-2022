13173b032030bf04cf57c1d6b4fe116a32aad00b
Ivo.araujo <86474505+ivoaraujo17@users.noreply.github.com>
2022-11-27 09:38:58 -0300

    arquivos duplicados excluidos

--- funcoes.h
+++
@@ -1,7 +0,0 @@
-/* Aqui listem as funções de funcoes.c
-exemplo:
-int soma(a, b)
-int subtracao(a, b)
-
-elas seram usadas como referencia para acessar a funcao em funcoes.c quando chamada em outro arquivo
-*/
